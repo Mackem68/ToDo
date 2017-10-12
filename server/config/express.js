09c171e3eee9a73397d0ec0e29f2704a02d780bd
Mackem68 <jsellers380@gmail.com>
2017-10-11 19:50:01 -0500

    initial comit

---
+++ server/config/express.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var morgan = require ('morgan');
+var logger = require('./logger');
+//var bodyParser = require ('body-parser');
+//var mongoose = require ('mongoose');
+//var bluebird = required ('bluebird');
+//var glob = required ('glob');
+
+module.exports = function (app, config) {
+
+  //logger.log("Loading Mongoose functionality");
+  //mongoose.Promise = require('bluebird’);
+  //mongoose.connect(config.db, {useMongoClient: true});
+  //var db = mongoose.connection;
+  //db.on('error', function () {
+  //  throw new Error('unable to connect to database at ' + config.db);
+  //});
+  //mongoose.set('debug', true);
+	//mongoose.connection.once('open', function callback() {
+	//	logger.log("Mongoose connected to the database");
+	//});
+
+
+  app.use (morgan('dev'));
+
+  app.use(function (req, res, next) {
+    console.log('Request from ' + req.connection.remoteAddress);
+    next();
+  });  
+  app.use(express.static(config.root + '/public'));
+  
+    app.use(function (req, res) {
+      res.type('text/plan');
+      res.status(404);
+      res.send('404 Not Found');
+    });
+  
+    app.use(function (err, req, res, next) {
+      console.error(err.stack);
+      res.type('text/plan');
+      res.status(500);
+      res.send('500 Sever Error');
+    });
+  
+    console.log("Starting application");
+  
+  };
+  
+  
