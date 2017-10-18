febb73b4ea636726a4a40d70eddecec726161030
Mackem68 <jsellers380@gmail.com>
2017-10-18 18:57:25 -0500

    express

--- server/config/express.js
+++ server/config/express.js
@@ -8,6 +8,8 @@ var glob = require ('glob');
 
 module.exports = function (app, config) {
 
+ // app.use(cors());
+
   logger.log("Loading Mongoose functionality");
   mongoose.Promise = require('bluebird');
   mongoose.connect(config.db, {useMongoClient: true});
@@ -16,26 +18,39 @@ module.exports = function (app, config) {
     throw new Error('unable to connect to database at ' + config.db);
   });
 
+  if(process.env.NODE_ENV !== 'test') {    
+  app.use (morgan('dev'));
+
+
   mongoose.set('debug', true);
   mongoose.connection.once('open', function callback() {
 		logger.log("Mongoose connected to the database");
 	});
-
-  app.use (morgan('dev'));
-
-  app.use(function (req, res, next) {
-    console.log('Request from ' + req.connection.remoteAddress);
-    next();
-  });  
-
-var routes = require ('./routes')(app);
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({
       extended: true
     }));
   
+    var models = glob.sync(config.root + '/app/models/*.js');
+    models.forEach(function (model) {
+      require(model);
+    });
+  
+  var controllers = glob.sync(config.root + '/app/controllers/*.js');
+    controllers.forEach(function (controller) {
+      require(controller)(app,config);
+    });
+    
+  app.use(function (req, res, next) {
+    logger.log('Request from ' + req.connection.remoteAddress);
+    next();
+  });  
 
+
+
+
+  
   app.use(express.static(config.root + '/public'));
   
     app.use(function (req, res) {
@@ -45,12 +60,13 @@ var routes = require ('./routes')(app);
     });
   
     app.use(function (err, req, res, next) {
-      console.error(err.stack);
+      logger.error(err.stack);
       res.type('text/plan');
       res.status(500);
       res.send('500 Sever Error');
     });
   
-    console.log("Starting application");
+    logger.log("Starting application");
   
   };
+}
